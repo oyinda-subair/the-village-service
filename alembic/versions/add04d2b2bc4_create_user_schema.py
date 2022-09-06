364ecfc6b4395ac25e612a0e38dff9c648460b91
Oyindamola Subair <oyinda.subair@gmail.com>
2022-09-06 04:41:04 +0100

    Set-up-database (#3)
    * Create docker scripts
    
    * Add alembic
    
    * Initialize db local and on docker

---
+++ alembic/versions/add04d2b2bc4_create_user_schema.py
@@ -0,0 +1,37 @@
+"""create user schema
+
+Revision ID: add04d2b2bc4
+Revises:
+Create Date: 2022-09-01 13:02:25.321894
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'add04d2b2bc4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "user",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("first_name", sa.String(length=256), nullable=True),
+        sa.Column("surname", sa.String(length=256), nullable=True),
+        sa.Column("email", sa.String(), nullable=False),
+        sa.Column("is_superuser", sa.Boolean(), nullable=True),
+        sa.Column("hashed_password", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_index(op.f("ix_user_email"), "user", ["email"], unique=False)
+    op.create_index(op.f("ix_user_id"), "user", ["id"], unique=False)
+
+
+def downgrade() -> None:
+    op.drop_index(op.f('ix_user_id'), table_name='user')
+    op.drop_index(op.f('ix_user_email'), table_name='user')
+    op.drop_table('user')
---
+++ app/db/base.py
@@ -0,0 +1,2 @@
+from db.config import Base  # noqa
+from models.user import User  # noqa
