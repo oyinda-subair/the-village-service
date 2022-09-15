b94d0acb6725128ae4ae0194aaf673eb6416ef97
oyinda-subair <oyinda.subair@gmail.com>
2022-09-15 15:21:23 +0100

    Change user endpoint prefix

--- app/api/routes.py
+++ app/api/routes.py
@@ -4,4 +4,4 @@ from app.api.v1 import user
 
 
 api_router = APIRouter()
-api_router.include_router(user.router, prefix="/user", tags=["user"])
+api_router.include_router(user.router, prefix="/users", tags=["users"])
