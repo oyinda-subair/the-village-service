63f641307b0c779b61dc38def101b5954b5e918c
oyinda-subair <oyinda.subair@gmail.com>
2022-09-23 12:29:35 +0100

    Create test specs for update and delete endpoints

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, APIRouter
+import time
+from fastapi import FastAPI, APIRouter, Request
 
 from app.api.routes import api_router
 from app.core.settings import settings
@@ -16,6 +17,14 @@ def root() -> dict:
     """
     return {"msg": "Hello, World!"}
 
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
 app.include_router(root_router)
--- app/tests/api/test_user.py
+++ app/tests/api/test_user.py
@@ -1,8 +1,8 @@
-import json
-from urllib import response
 from app import crud
 from app.core.settings import settings
 from app.schemas.user import UserCreate
+from app.tests import UserFactory, generate_token
+from app.core.security import get_password_hash
 
 
 def test_create_user(client):
@@ -15,7 +15,7 @@ def test_create_user(client):
     # Then
     assert response.status_code == 201
     for key in data.keys():
-        assert key in ["id", "first_name", "surname", "email", "is_superuser"]
+        assert key in ["id", "first_name", "surname", "email", "is_superuser", "created_at", "updated_at"]
 
 
 def test_user_login(client, session):
@@ -32,3 +32,63 @@ def test_user_login(client, session):
     assert response.status_code == 200
     for key in data.keys():
         assert key in ["access_token", "token_type"]
+
+
+def test_current_user(client, session):
+    password = "password1"
+    user = UserFactory(hashed_password=get_password_hash(password))
+    assert user
+
+    data = generate_token(user)
+
+    header = {
+        'Authorization': 'Bearer {}'.format(data['access_token'])
+    }
+
+    response = client.get(f"{settings.API_V1_STR}/users/me", headers=header)
+    result = response.json()
+
+    assert response.status_code == 200
+    assert result["first_name"] == user.first_name
+
+
+def test_update_user(client):
+    password = "password1"
+    user = UserFactory(hashed_password=get_password_hash(password))
+    assert user
+
+    data = generate_token(user)
+
+    header = {
+        'Authorization': 'Bearer {}'.format(data['access_token'])
+    }
+
+    user_update = {"surname": "Test"}
+
+    response = client.put(f"{settings.API_V1_STR}/users/{user.id}", json=user_update, headers=header)
+    result = response.json()
+
+    assert response.status_code == 200
+    assert result["surname"] != user.surname
+    assert result["surname"] == user_update["surname"]
+
+
+def test_delete_user(client, session):
+    password = "password1"
+    user = UserFactory(hashed_password=get_password_hash(password))
+    assert user
+
+    data = generate_token(user)
+
+    header = {
+        'Authorization': 'Bearer {}'.format(data['access_token'])
+    }
+
+    response = client.delete(f"{settings.API_V1_STR}/users/{user.id}", headers=header)
+
+    assert response.status_code == 204
+
+    from app.models.user import User
+    deleted_user = session.query(User).filter(User.email == user.email).first()
+
+    assert deleted_user == None
