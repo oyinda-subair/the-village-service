89cc204268f269334a743863e4dde33179d1b3d7
oyinda-subair <oyinda.subair@gmail.com>
2022-09-29 00:37:58 +0100

    Add admin routes with superuser dependency

---
+++ app/api/v1/admin.py
@@ -0,0 +1,67 @@
+from typing import List, Any
+from fastapi import APIRouter, Depends, status
+
+from app import controller
+from app import schemas
+from app.api import deps
+from app.core.exception_handler import CustomException
+from app.schemas.common import DataResponseModel
+
+from sqlalchemy.orm.session import Session
+from loguru import logger
+
+router = APIRouter()
+
+
+# GET admin/users/ (all users)
+@router.get("/users", response_model=DataResponseModel[List[schemas.User]], status_code=200)
+def fetch_all_users(
+        *,
+        db: Session = Depends(deps.get_db),
+        skip: int = 0, limit: int = 100) -> Any:
+    users = controller.admin.get_multi(db, skip=skip, limit=limit)
+    return DataResponseModel(success=True, data=users)
+
+# GET admin/user/:id
+
+
+@router.get("/user/{user_id}", response_model=DataResponseModel[schemas.User], status_code=200)
+def fetch_user(
+        *, db: Session = Depends(deps.get_db),
+        user_id: int) -> Any:
+    user = controller.admin.get(db, user_id)
+    return DataResponseModel(success=True, data=user)
+
+# PUT admin/user/:id
+
+
+@router.put("/user/{user_id}", response_model=schemas.User, status_code=200)
+def user_update(
+        *, db: Session = Depends(deps.get_db),
+        user_id: int,
+        user_in: schemas.UserUpdate) -> Any:
+    user = _get_user(db, user_id)
+
+    updated_user = controller.user.update(db=db, db_obj=user, obj_in=user_in)
+    return updated_user
+
+# DELETE admin/user/:id
+
+
+@router.delete("/user/{user_id}", status_code=204)
+def delete_user(
+        *, db: Session = Depends(deps.get_db),
+        user_id: int) -> Any:
+    _ = _get_user(db, user_id)
+    return controller.user.delete(db=db, id=user_id)
+
+
+def _get_user(db: Session, user_id: int) -> Any:
+    user = controller.user.get(db, id=user_id)
+    if not user:
+        logger.error(f"User with ID: {user_id} not found.")
+        raise CustomException(
+            status.HTTP_404_NOT_FOUND, f"User with ID: {user_id} not found."
+        )
+
+    return user
