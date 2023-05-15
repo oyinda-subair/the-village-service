8f7f5d60cd5085ea9e3fd6731cf1f41724486f9e
oyinda-subair <oyinda.subair@gmail.com>
2023-05-15 10:47:19 +0100

    wrap controller in try/exception

--- app/api/v1/user.py
+++ app/api/v1/user.py
@@ -20,15 +20,20 @@ router = APIRouter()
 
 @router.get("/me", response_model=schemas.ShortUserResponse)
 def read_users_me(current_user: User = Depends(deps.get_current_user)):
-    user = current_user
-    data = ShortUserResponse(
-        first_name=user.first_name,
-        surname=user.surname,
-        email=user.email,
-        is_superuser=user.is_superuser,
-        role=user.role
-    )
-    return data
+    try:
+        user = current_user
+        data = ShortUserResponse(
+            id=user.id,
+            first_name=user.first_name,
+            surname=user.surname,
+            email=user.email,
+            is_superuser=user.is_superuser,
+            role=user.role
+        )
+        return data
+    except Exception as e:
+        logger.error(f"Internal Server Error #{e}")
+        raise CustomException(code=500, message="Oop! An Error has occured")
 
 
 @router.put("/{user_id}", response_model=schemas.User, status_code=200)
@@ -36,30 +41,42 @@ def user_update(
         *, db: Session = Depends(deps.get_db),
         user_id: str,
         user_in: schemas.UserUpdate, current_user: User = Depends(deps.get_current_user)) -> Any:
-    user = _get_user(db, uuid.UUID(user_id), current_user.id)
-    updated_user = controller.user.update(db=db, db_obj=user, obj_in=user_in)
-    return updated_user
+    try:
+        user = _get_user(db, uuid.UUID(user_id), current_user.id)
+        updated_user = controller.user.update(db=db, db_obj=user, obj_in=user_in)
+        return updated_user
+    except Exception as e:
+        logger.error(f"Internal Server Error #{e}")
+        raise CustomException(code=500, message="Oop! An Error has occured")
 
 
 @router.delete("/{user_id}", status_code=204)
 def delete_user(
         *, db: Session = Depends(deps.get_db),
         user_id: str, current_user: User = Depends(deps.get_current_user)) -> Any:
-    _ = _get_user(db, uuid.UUID(user_id), current_user.id)
-    return controller.user.delete(db=db, id=uuid.UUID(user_id))
+    try:
+        _ = _get_user(db, uuid.UUID(user_id), current_user.id)
+        return controller.user.delete(db=db, id=uuid.UUID(user_id))
+    except Exception as e:
+        logger.error(f"Internal Server Error #{e}")
+        raise CustomException(code=500, message="Oop! An Error has occured")
 
 
 def _get_user(db: Session, id: uuid.UUID, current_user_id: uuid.UUID) -> Any:
-    user = controller.user.get(db, id=current_user_id)
-    if not user:
-        logger.error(f"User with ID: {current_user_id} not found.")
-        raise CustomException(
-            status.HTTP_404_NOT_FOUND, f"User with ID: {current_user_id} not found."
-        )
+    try:
+        user = controller.user.get(db, id=current_user_id)
+        if not user:
+            logger.error(f"User with ID: {current_user_id} not found.")
+            raise CustomException(
+                status.HTTP_404_NOT_FOUND, f"User with ID: {current_user_id} not found."
+            )
 
-    if id != current_user_id:
-        logger.error(f"You can only update your profile.")
-        raise CustomException(
-            status.HTTP_403_FORBIDDEN, f"You can only update your profile."
-        )
-    return user
+        if id != current_user_id:
+            logger.error(f"You can only update your profile.")
+            raise CustomException(
+                status.HTTP_403_FORBIDDEN, f"You can only update your profile."
+            )
+        return user
+    except Exception as e:
+        logger.error(f"Internal Server Error #{e}")
+        raise CustomException(code=500, message="Oop! An Error has occured")
